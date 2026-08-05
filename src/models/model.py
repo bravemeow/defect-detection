557eec5efaf07fce508398cc6da1ffc0bdfab569
Juhwan Oh <bravemeow@MacBook-Air-6.local>
2026-08-05 13:46:00 -0400

    feat: add SimpleCNN model and classifier layers

---
+++ src/models/model.py
@@ -0,0 +1,48 @@
+
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=16,
+            kernel_size=3
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=3
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3
+        )
+
+        # relu/pool could be relu2 relu3... if different activation functions used, but now ReLU is only used.
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.flatten = nn.Flatten(start_dim=1)
+        self.linear = nn.Linear(43264, 2)  # 43264 is computed manually for learning purpose.
+
+        
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.flatten(x)
+        x = self.linear(x)
+        return x
--- src/train.py
+++ src/train.py
@@ -1,7 +1,17 @@
 from data.dataloader import create_loaders
-
+from models.model import SimpleCNN
 
 train_loader, val_loader, test_loader = create_loaders(
     data_dir = ("data/processed"),
     batch_size = 32,
 )
+
+model = SimpleCNN()
+
+for images, label in train_loader:
+    
+    output = model(images)    
+    print(images.shape)
+    print(output.shape)
+    print(output.max())
+    break
